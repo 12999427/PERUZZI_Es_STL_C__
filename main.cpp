4193eb8546eb966e8ae270137152e6d45b9720a0
12999427 <12999427@itisrossi.vi.it>
2026-02-09 13:25:58 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+void main0()
+{
+    std::array<int, 10> = new({20, 50, -10, -40, 80, -30, 70, 55, 75, 5});
+}
+
+void main1()
+{
+
+}
+
+void main2()
+{
+
+}
+
+void main3()
+{
+
+}
+
+int main()
+{
+    int numEsercizio = 0;
+
+    switch (numEsercizio)
+    {
+    case 0:
+        main0();
+        break;
+    case 1:
+        main1();
+        break;
+    case 2:
+        main2();
+        break;
+    case 3:
+        main3();
+        break;
+    }
+
+    return 0;
+}
